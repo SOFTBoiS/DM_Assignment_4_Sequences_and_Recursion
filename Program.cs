83e61181dc32a558a7b7fbbe07eeadcc5776a371
Sebastian Lundsgaard-Larsen <s_elementos@hotmail.com>
2020-10-05 14:15:02 +0200

    made whole project

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static DMAssignment4SequencesAndRecursion.Rationals;
+
+namespace DMAssignment4SequencesAndRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            long num = 25;
+
+            var rationalNum = Rationals.Rational(num);
+
+            Console.WriteLine($"The {num}th rational number in the sequence is: " + rationalNum);
+
+            Rationals rationals = new Rationals();
+            var iterations = 10;
+            var current = 0;
+            Console.WriteLine("Loop through the iterable from 0-10 with an Enumerable");
+            foreach (var x in rationals)
+            {
+                Console.WriteLine($"The {current++}th number is {x}");
+                if (current >= iterations) break;
+            }
+
+
+            current = 0;
+            var enumerator = rationals.GetEnumerator();
+            Console.WriteLine("Loop through the iterable from 0-10 with an Enumerator");
+            while (enumerator.MoveNext() && current < iterations)
+            {
+                Console.WriteLine($"The {current++}st number is {enumerator.Current}");
+            }
+        }
+
+
+
+
+        //for (int i = 0; i < 11; i++)
+        //{
+
+        //}
+
+
+        //var test = rational.GetEnumerator();
+    }
+}
+
---
+++ Rationals.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMAssignment4SequencesAndRecursion
+{
+    class Rationals : IEnumerable<string>
+    {
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new RationalEnumerator();
+        }
+
+        public static string Rational(long n)
+        {
+            // The diagonal line number lₙ from n
+            long ln = (long)((-1 + Math.Sqrt(1 + 8 * n)) / 2);
+            // Line including n starts at sₙ:
+            var sn = ln * (ln + 1) / 2;
+            var numerator = CalculateNumerator(n, sn);
+            var denominator = CalculateDenominator(n, sn, ln);
+
+            return numerator + "/" + denominator;
+        }
+
+        public static long CalculateNumerator(long n, long sn)
+        {   
+            return n - sn + 1;
+        }
+        public static long CalculateDenominator(long n, long sn, long ln)
+        {
+            return ln - (n - sn) + 1;
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (long i = 0; i < long.MaxValue; i++)
+            {
+                yield return Rational(i);
+            }
+        }
+
+        class RationalEnumerator : IEnumerator<string>
+        {
+            public string Current => Rational(_currIndex);
+            private long _currIndex { get; set; }
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++_currIndex < long.MaxValue;
+            }
+
+            public void Reset()
+            {
+                _currIndex = -1;
+            }
+        }
+    }
+}
